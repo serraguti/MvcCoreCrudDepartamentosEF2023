8f3f2618439d38ce682e6b0fc61d2eb185ed6d02
serraguti <pacoserranox@gmail.com>
2023-02-16 14:20:18 +0100

    Add project files.

---
+++ MvcCoreCrudDepartamentosEF/Data/DepartamentosContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MvcCoreCrudDepartamentosEF.Models;
+
+namespace MvcCoreCrudDepartamentosEF.Data
+{
+    public class DepartamentosContext: DbContext
+    {
+        public DepartamentosContext(DbContextOptions<DepartamentosContext> options)
+            : base(options) { }
+
+        public DbSet<Departamento> Departamentos { get; set; }
+    }
+}
---
+++ MvcCoreCrudDepartamentosEF/Repositories/RepositoryDepartamentos.cs
@@ -0,0 +1,30 @@
+﻿using MvcCoreCrudDepartamentosEF.Data;
+using MvcCoreCrudDepartamentosEF.Models;
+
+namespace MvcCoreCrudDepartamentosEF.Repositories
+{
+    public class RepositoryDepartamentos
+    {
+        private DepartamentosContext context;
+
+        public RepositoryDepartamentos(DepartamentosContext context)
+        {
+            this.context = context;
+        }
+
+        public List<Departamento> GetDepartamentos()
+        {
+            var consulta = from datos in this.context.Departamentos
+                           select datos;
+            return consulta.ToList();
+        }
+
+        public Departamento FindDepartamento(int iddepartamento)
+        {
+            var consulta = from datos in this.context.Departamentos
+                           where datos.IdDepartamento == iddepartamento
+                           select datos;
+            return consulta.FirstOrDefault();
+        }
+    }
+}
